2d97127d900fa6a551cf8789d340ef6a63b33e4c
SOUMYA <goldenluvathena.01@gmail.com>
2024-02-15 20:07:29 +0530

    NEW LABEL

--- AppointMentModule/UpdateAppointStatus.java
+++ AppointMentModule/UpdateAppointStatus.java
@@ -18,7 +18,7 @@ public class UpdateAppointStatus implements ActionListener {
     AppointMent appoint;
     DBOperation DBO;
     MedPannel infoPannel;
-    JLabel PID, Name, Number, Status_lable, time;
+    JLabel PID, Name, Number, Status_lable, time, PID_L, Name_L, Number_L, time_L;
     JComboBox<String> StatusOpt;
     MedEaseBtn Update;
     String opt[] = {
@@ -31,8 +31,7 @@ public class UpdateAppointStatus implements ActionListener {
             "CANCEL",
     };
 
-    public UpdateAppointStatus(AppointMent appoint, DBOperation dbo, MedCreatePatient pt) 
-    {
+    public UpdateAppointStatus(AppointMent appoint, DBOperation dbo, MedCreatePatient pt) {
         this.appoint = appoint;
         this.DBO = dbo;
         this.pt = pt;
@@ -50,20 +49,40 @@ public class UpdateAppointStatus implements ActionListener {
         PID.setBounds(10, 10, 100, 30);
         infoPannel.add(PID);
 
+        PID_L = new JLabel("PID");
+        PID_L.setFont(GUIUtil.TimesBoldS2);
+        PID_L.setBounds(110, 10, 200, 30);
+        infoPannel.add(PID_L);
+
         Name = new JLabel("NAME");
         Name.setFont(GUIUtil.TimesBoldS2);
         Name.setBounds(10, 50, 100, 30);
         infoPannel.add(Name);
+
+        Name_L = new JLabel("NAME");
+        Name_L.setFont(GUIUtil.TimesBoldS2);
+        Name_L.setBounds(110, 50, 200, 30);
+        infoPannel.add(Name_L);
 
         Number = new JLabel("NUMBER");
         Number.setFont(GUIUtil.TimesBoldS2);
         Number.setBounds(10, 90, 100, 30);
         infoPannel.add(Number);
 
+        Number_L = new JLabel("NUMBER");
+        Number_L.setFont(GUIUtil.TimesBoldS2);
+        Number_L.setBounds(10, 90, 100, 30);
+        infoPannel.add(Number_L);
+
         time = new JLabel("TIME");
         time.setFont(GUIUtil.TimesBoldS2);
         time.setBounds(10, 130, 100, 30);
         infoPannel.add(time);
+
+        time_L = new JLabel("TIME");
+        time_L.setFont(GUIUtil.TimesBoldS2);
+        time_L.setBounds(10, 130, 100, 30);
+        infoPannel.add(time_L);
 
         Status_lable = new JLabel("STATUS");
         Status_lable.setFont(GUIUtil.TimesBoldS2);
@@ -76,8 +95,8 @@ public class UpdateAppointStatus implements ActionListener {
         infoPannel.add(StatusOpt);
 
         Update = new MedEaseBtn(GUIUtil.Dark_BLue, GUIUtil.BlueColor, null, 5);
-        Update.setBounds(110, 210, 80, 30);
-        Update.setText("LOGIN");
+        Update.setBounds(120, 210, 120, 30);
+        Update.setText("UPDATE");
         Update.setForeground(Color.white);
         Update.setFont(GUIUtil.TimesBold);
         // Update.addActionListener();
@@ -89,10 +108,7 @@ public class UpdateAppointStatus implements ActionListener {
         StatusOpt.setBounds(110, 170, 150, 25);
         StatusOpt.setFont(GUIUtil.TimesBold);
         infoPannel.add(StatusOpt);
-    }    
->>>>>>> af3647dadebf32314bba6cb4b8e72f8e08dc896d
-
-    
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
