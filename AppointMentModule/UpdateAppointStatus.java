8b6d1eff1c77cf94474d2d658ad33d4d743b76ca
SOUMYA <goldenluvathena.01@gmail.com>
2024-02-15 07:36:52 +0530

    UPDATE APPOINTMENT STATUS

--- AppointMentModule/UpdateAppointStatus.java
+++ AppointMentModule/UpdateAppointStatus.java
@@ -17,7 +17,7 @@ public class UpdateAppointStatus implements ActionListener {
     AppointMent appoint;
     DBOperation DBO;
     MedPannel infoPannel;
-    JLabel PID, Name, Number, Status_lable;
+    JLabel PID, Name, Number, Status_lable, time;
     JComboBox<String> StatusOpt;
     MedEaseBtn Update;
     String opt[] = {
@@ -57,6 +57,22 @@ public class UpdateAppointStatus implements ActionListener {
         Number.setFont(GUIUtil.TimesBoldS2);
         Number.setBounds(10, 90, 100, 30);
         infoPannel.add(Number);
+
+        // time = new JLabel("TIME");
+        // time.setFont(GUIUtil.TimesBoldS2);
+        // time.setBounds(10, 130, 100, 30);
+        // infoPannel.add(time);
+
+        // Status_lable = new JLabel("STATUS");
+        // Status_lable.setFont(GUIUtil.TimesBoldS2);
+        // Status_lable.setBounds(10, 170, 100, 30);
+        // infoPannel.add(Status_lable);
+
+        // String status[] = {"-","IN","OUT"}
+        // StatusOpt = new JComboBox<String>(status);
+        // StatusOpt.setBounds(50, 210, 50, 25);
+        // StatusOpt.setFont(GUIUtil.TimesBold);
+        // infoPannel.add(StatusOpt);
 
     }
 
