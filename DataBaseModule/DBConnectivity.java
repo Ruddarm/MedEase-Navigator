af10002e26b368abaa822e5b0eabbf29ceb7316a
SOUMYA VINOD <goldenluvathena.01@gmail.com>
2024-03-03 17:11:25 +0530

    setmsgs

--- DataBaseModule/DBConnectivity.java
+++ DataBaseModule/DBConnectivity.java
@@ -35,7 +35,7 @@ public class DBConnectivity {
             return true;
         } catch (SQLException connectionError) {
             // System.out.println("ik");
-            DbNotify.setMsg("Unnable To Connect ", -1);
+            DbNotify.setMsg("Unable To Connect ", -1);
             return false;
         }
 
@@ -73,7 +73,7 @@ public class DBConnectivity {
                         CreateUtilitytable()) {
                     DBCon.commit();
                 } else {
-                    DbNotify.setMsg("Uanablee to create tables", -1);
+                    DbNotify.setMsg("Unable to create tables", -1);
                 }
                 return true;
             } catch (SQLException error) {
